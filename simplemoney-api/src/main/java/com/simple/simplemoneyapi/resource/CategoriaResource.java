2cc5fa6f1cde982233da6295a4a2905f06b25302
luan-data-scientist <luanrochaprates@gmail.com>
2019-08-06 11:25:02 -0300

    Get em apenas uma categoria

--- simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
+++ simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,11 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return ((JpaRepository<Categoria, Long>) categoriaRepository).findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findOne(codigo);
 	}
 	
 	@PostMapping
