8875123e22e62a62aaa7dd80d35d0dfef9634871
luan-data-scientist <luanrochaprates@gmail.com>
2019-08-06 10:40:57 -0300

    Salvando categoria via POST

--- simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
+++ simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simple.simplemoneyapi.model.Categoria;
@@ -21,5 +25,11 @@ public class CategoriaResource {
 	@GetMapping
 	public List<Categoria> listar(){
 		return ((JpaRepository<Categoria, Long>) categoriaRepository).findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void criar(@RequestBody Categoria categoria) {
+		categoriaRepository.save(categoria);	
 	}
 }
