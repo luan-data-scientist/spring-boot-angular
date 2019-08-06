f392477dae1a385ae9dcd2363050db8440d40a6f
luan-data-scientist <luanrochaprates@gmail.com>
2019-08-06 10:57:06 -0300

    Retornando o local e no corpo da resposta o objeto inserido
    Retornando o local onde acessar o objeto e no corpo da resposta os dados do objeto inserido

--- simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
+++ simplemoney-api/src/main/java/com/simple/simplemoneyapi/resource/CategoriaResource.java
@@ -1,16 +1,21 @@
 package com.simple.simplemoneyapi.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.simple.simplemoneyapi.model.Categoria;
 import com.simple.simplemoneyapi.repository.CategoriaRepository;
@@ -28,8 +33,12 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void criar(@RequestBody Categoria categoria) {
-		categoriaRepository.save(categoria);	
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	 	Categoria categoriaSalva = categoriaRepository.save(categoria);	
+	 	
+	 	URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+	 			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+	 	
+	 	return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 }
